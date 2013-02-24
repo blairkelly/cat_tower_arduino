2dc34880d0f9789b617436da9aa0cc98ae76b14c
blairkelly <blairkelly1@gmail.com>
2013-02-24 17:02:07 -0500

    Many advancements.

--- cat_tower_arduino.ino
+++ cat_tower_arduino.ino
@@ -23,16 +23,22 @@ boolean OFF = 0;
 boolean OPEN = 1;
 boolean CLOSED = 0;
 boolean lightis = OFF;
-unsigned long lightondelay = 120000;   //minimum duration for which the light is kept on.
+boolean xbeeis = ON;
+unsigned long xbeepwronat = millis();
+int xbeepwrondelay = 1000; //length of time to wait for xbee to power up before writing data.
+String databuffer = "";      //data queued to be sent through xbee once it's ready.
+unsigned long xbeeondelay = 45000;   //minimum length of time to keep xbee on.
+unsigned long xbeeusedat = millis();   //when was the last time the xbee was used?
+unsigned long lightondelay = 180000;   //minimum duration for which the light is kept on.
 unsigned long lightwentonat = millis();   //what time did the light go on at?
-unsigned long checkindelay = 60000;   //check-in interval
-unsigned long checkintime = millis() + checkindelay;
 int dbd_catdoor = 100;   //amount of time it takes to consider the cat door "opened"
 unsigned long dbd_cat_in = millis();  //cat in-door debounce time
 unsigned long dbd_cat_out = millis();  //cat out-door debounce time
 unsigned long dbd_maindoor = millis();  //maindoor debounce time
 unsigned long dbd_fooddoor = millis();  //fooddoor debounce time
 unsigned long dbd_walle = millis();  //walle debounce time
+unsigned long wallelastsaw = millis();
+int wewblatd = 8000;  //Wall-E Will Be Lonely After This Delay
 boolean dbd_cat_in_d = false; //cat in door debouncing state.
 boolean dbd_cat_out_d = false; //cat out door debouncing state.
 boolean dbd_maindoor_d = false; //maindoor debouncing state.
@@ -60,21 +66,41 @@ void setup() {
   digitalWrite(pinXbeePwr, ON);
   digitalWrite(pinLed, OFF);
   //start serial
+  delay(xbeepwrondelay);
   Serial.begin(57600);
-  delay(500);
-  Serial.println("Ready");
-  delay(500);
+  delay(xbeepwrondelay);
+  Serial.println("R1");
+  delay(xbeepwrondelay);
   digitalWrite(pinXbeePwr, OFF);
+  xbeeis = OFF;
 }
 
+void checkbee() {
+  if(!digitalRead(pinXbeePwr)) {
+    //xbee is off. Turn it on.
+    digitalWrite(pinXbeePwr, ON);
+    xbeeis = ON;
+    xbeepwronat = millis();  //record the time at which the xbee was powered on.
+    xbeeusedat = millis();
+  }
+}
+void pfwrite() {
+  if(!databuffer.equals("")) {
+    if(!xbeeis) {
+      checkbee();
+    } else if(((millis() - xbeepwronat) > xbeepwrondelay) && xbeeis) {
+      //it's ok to write to the xbee
+      Serial.println(databuffer);
+      xbeeusedat = millis();
+      databuffer = "";
+    }
+  }
+}
 void pfs(String pthis) {
-  Serial.print(pthis);
+  databuffer = databuffer + pthis;
 }
 void pfi(int pthis) {
-  Serial.print(pthis);
-}
-void pfl() {
-  Serial.println(" ");
+  databuffer = databuffer + String(pthis);
 }
 void printsensors() {
   if(ps) {
@@ -88,15 +114,8 @@ void printsensors() {
     pfi(analogRead(pinWalle));
     pfs(", pinFoodDoor");
     pfi(analogRead(pinFoodDoor));
-    pfl();
     delay(111);
   }
-}
-
-void checkin() {
-  // pfs("catsinside = ");
-  // pfi(catsinside);
-  // pfl();
 }
 
 void kittylight(boolean state) {
@@ -105,16 +124,27 @@ void kittylight(boolean state) {
     lightis = ON;
     delay(77); //delay in order to avoid bad sensor readings due to power fluctuation.
     lightwentonat = millis();
+    pfs("l");
+    pfi(1);
   } else if (!state && digitalRead(pinKittyLight)) {
     digitalWrite(pinKittyLight, OFF);
     lightis = OFF;
+    pfs("l");
+    pfi(0);
   }
 }
 
-void checklight() {
+void checkpower() {
+  //checks light and xbee power.
   if(lightis) {
     if(((millis() - lightwentonat) > lightondelay) && !maindoor) {
       kittylight(OFF);
+    }
+  }
+  if(xbeeis) {
+    if((millis() - xbeeusedat) > xbeeondelay) {
+      digitalWrite(pinXbeePwr, OFF);
+      xbeeis = OFF;
     }
   }
 }
@@ -131,6 +161,8 @@ void dodoors() {
         dbd_cat_in_d = false;
         catindoor = OPEN;
         catsinside++;
+        pfs("c");
+        pfi(catsinside);
         kittylight(ON);
       }
     }
@@ -157,6 +189,8 @@ void dodoors() {
         dbd_cat_out_d = false;
         catoutdoor = OPEN;
         catsinside--;
+        pfs("c");
+        pfi(catsinside);
       }
     }
   } else {
@@ -174,15 +208,18 @@ void dodoors() {
   //read maintenance door
   if(!ignoremain) {
     if(analogRead(pinMainDoor) > 490) {
-      //cat door appears to be open
+      //maintainance door appears to be open
       if(!dbd_maindoor_d) {
         dbd_maindoor = millis() + dbd_catdoor;
         dbd_maindoor_d = true;
       } else if (dbd_maindoor_d) {
-        if(millis() > dbd_maindoor) {
+        if((millis() > dbd_maindoor) && !maindoor) {
+          closewalle();
           dbd_maindoor_d = false;
           maindoor = OPEN;
           kittylight(ON);
+          pfs("m");
+          pfi(1);
         }
       }
     } else {
@@ -194,67 +231,86 @@ void dodoors() {
           dbd_maindoor_d = false;
           maindoor = CLOSED;
           kittylight(OFF);
+          pfs("m");
+          pfi(0);
         }
       }
     }
   }
 
   //read food door
-  if(analogRead(pinFoodDoor) > 120) {
-    //cat door appears to be open
+  if(analogRead(pinFoodDoor) > 50) {
+    //food and supplies door appears to be open.
     if(!dbd_fooddoor_d && !fooddoor) {
-      dbd_fooddoor = millis() + dbd_catdoor;
+      dbd_fooddoor = millis() + 500;
       dbd_fooddoor_d = true;
     } else if (dbd_fooddoor_d) {
       if((millis() > dbd_fooddoor) && !fooddoor) {
         dbd_fooddoor_d = false;
         fooddoor = OPEN;
-        pfs("FoodDoor OPEN");
-        pfl();
+        pfs("f");
+        pfi(1);
       }
     }
   } else {
     if(fooddoor) {
       if(!dbd_fooddoor_d) {
         dbd_fooddoor_d = true;
-        dbd_fooddoor = millis() + dbd_catdoor;
+        dbd_fooddoor = millis() + 500;
       } else if (millis() > dbd_fooddoor) {
         dbd_fooddoor_d = false;
         fooddoor = CLOSED;
-        pfs("FoodDoor CLOSED");
-        pfl();
+        pfs("f");
+        pfi(0);
       }
     }
   }
 }
 
+void closewalle() {
+  dbd_walle_d = false;
+  walle = CLOSED;
+  //kittylight(OFF);
+  pfs("W");
+  pfi(0);
+}
 void checkwalle() {
-  //read Wall-E
-  if(analogRead(pinWalle) > 333) {
-    //it appears there's a cat in front of Wall-E
-    if(!dbd_walle_d && !walle) {
-      dbd_walle = millis() + 333;
-      dbd_walle_d = true;
-    } else if (dbd_walle_d) {
-      if((millis() > dbd_walle) && !walle) {
-        dbd_walle_d = false;
-        walle = true;
-        kittylight(ON);
-        pfs("Wall-E sees a kitty.");
-        pfl();
-      }
-    }
-  } else {
-    if(walle) {
-      if(!dbd_walle_d) {
+  if(!maindoor || ignoremain) {
+    int walleHigh = 311;
+    int walleMid = 190;
+    //read Wall-E
+    if(analogRead(pinWalle) > walleHigh) {
+      //it appears there's a cat in front of Wall-E
+      wallelastsaw = millis(); //the last time Wall-E saw something.
+      if(!dbd_walle_d && !walle) {
+        dbd_walle = millis() + 333;
         dbd_walle_d = true;
-        dbd_walle = millis() + 3333;
-      } else if (millis() > dbd_walle) {
-        dbd_walle_d = false;
-        walle = CLOSED;
-        //kittylight(OFF);
-        pfs("Wall-E can't see a kitty.");
-        pfl();
+      } else if (dbd_walle_d) {
+        if((millis() > dbd_walle) && !walle) {
+          dbd_walle_d = false;
+          walle = true;
+          kittylight(ON);
+          pfs("W");
+          pfi(1);
+        }
+      }
+    } else if ((analogRead(pinWalle) > walleMid) && (analogRead(pinWalle) <= walleHigh)) {
+      //do nothing. it's a grey zone. The proximity sensor seems to fluctuate quite a bit
+      //When something is put in front of it, the value can change even if the item doesn't
+      //move - perhaps I have a bad sensor. I'm not sure.
+    } else {
+      //Wall-E is low.
+      //here, we can specify how long it'll take before the Arduino decides
+      //a cat is no longer in the litterbox. I am guessing that a cat could
+      //be seen to be moving inside a litterbox at least every twelve seconds
+      //or so...
+      if(walle && ((millis() - wallelastsaw) > wewblatd)) {
+        if(!dbd_walle_d) {
+          dbd_walle_d = true;
+          dbd_walle = millis() + 5000;
+        } else if (millis() > dbd_walle) {
+          closewalle();
+        }
       }
     }
   }
@@ -262,7 +318,8 @@ void checkwalle() {
 
 void loop() {
   dodoors();
-  checklight();
   checkwalle();
   printsensors();
+  pfwrite();
+  checkpower();
 }
